ce917c82ddcdaf73f673958963e1cc56a3535d05
AlexanderCastroRuiz <firelife123456@gmail.com>
2023-10-18 15:20:25 -0300

    ola

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	// Crear un mapa para almacenar los apellidos de los jugadores y la descripcion.
 	diccionario := map[string]string{
 		"Messi": "Edad:36 \nNombre completo:Lionel Andres Messi Cuccittini \nApodo:La pulga \nFecha de nacimiento:24 de junio de 1987  \nEstatura:1.7 \nPeso:65 ",
 		"Basile": "Edad:79 \nNombre completo:Alfio Ruben Basile \nApodo:Coco Basile \nFecha de nacimiento: 1 de noviembre de 1943 \nEstatura:1.92 \nPeso:120 ",
@@ -14,16 +13,12 @@ func main() {
 		"Tagliafico": "Edad: 31\nNombre completo: Nicolás Alejandro \nApellido: Tagliafico\nApodo: taglia\nFecha de nacimiento: 31 de agosto de 1992\nEstatura: 172 cm\nPeso: 72 kg",
 	}
 
-	// Verificar si se proporciona un argumento (la palabra a buscar).
 	if len(os.Args) != 2 {
 		fmt.Println("Uso: tp26.exe apellido_jugador")
 		return
 	}
 
-	// Obtener el apellido proporcionado como argumento.
 	apellido := os.Args[1]
-
-	// Utilizar una declaración switch para buscar y mostrar la descripción.
 	switch descripcion := diccionario[apellido]; descripcion {
 	case "":
 		fmt.Printf("El jugador '%s' no se encuentra.\n", apellido)
