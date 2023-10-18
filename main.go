faa9edcacead2fedc0a08574d570e66c56cbc532
MatiasB235 <135553952+MatiasB235@users.noreply.github.com>
2023-10-18 14:25:24 -0300

    Update main.go

--- main.go
+++ main.go
@@ -9,9 +9,8 @@ func main() {
 	// Crear un mapa para almacenar los apellidos de los jugadores y la descripcion.
 	diccionario := map[string]string{
 		"Messi": "Edad:36 \nNombre completo:Lionel Andres Messi Cuccittini \nApodo:La pulga \nFecha de nacimiento:24 de junio de 1987  \nEstatura:1.7 \nPeso:65 ",
-		"Basile": "Edad:79 \nNombre completo:Alfio Ruben Basile \nApodo:Coco Basile \nFecha de nacimiento: 1 de noviembre de 1943 \nEstatura:1.92 \nPeso:120 ",
-		"Julian":     "Edad: 26\nNombre completo: Julian\nApellido: Álvarez\nApodo: araña\nFecha de nacimiento:  30 de enero de 2000\nEstatura:  170 cm\nPeso: 71 kg",
-		"Tagliafico": "Edad: 31\nNombre completo: Nicolás Alejandro \nApellido: Tagliafico\nApodo: taglia\nFecha de nacimiento: 31 de agosto de 1992\nEstatura: 172 cm\nPeso: 72 kg",
+		"MacAllister": "Edad: 24\nNombre completo:Alexis Mac Allister\nApodo: Macca\nFecha de nacimiento: 24 de diciembre de 1998\nEstatura: 1,76 m\nPeso: 72kg",
+		"Tagliafico": "Edad: 31\nNombre completo: Nicolás Alejandro Tagliafico\nApodo: Taglia\nFecha de nacimiento: 31 de agosto de 1992 \nEstatura: 1,72 m\nPeso: 65 kg",
 	}
 
 	// Verificar si se proporciona un argumento (la palabra a buscar).
