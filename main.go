bc788dfe85af17502091f84ad38a36d49d830638
Jose <josefuentesprofesor@gmail.com>
2023-10-19 15:37:12 -0300

    Correción de puntuación

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 	diccionario := map[string]string{
-		"Reus":         "Edad:34 \nNombre completo:Marco Reus \nApodo:Woody \nFecha de nacimiento:31 de mayo de 1989   \nEstatura:1.8 \nPeso:71 kg ",
-		"Foden":        "Edad:23 \nNombre completo:Philip Walter Foden \nApodo: foden \nFecha de nacimiento: 28 de mayo de 2000 \nEstatura:1.71 \nPeso:70 kg ",
+		"Reus":         "Edad: 34 \nNombre completo:Marco Reus \nApodo:Woody \nFecha de nacimiento:31 de mayo de 1989   \nEstatura:1.8 \nPeso:71 kg ",
+		"Foden":        "Edad: 23 \nNombre completo:Philip Walter Foden \nApodo: foden \nFecha de nacimiento: 28 de mayo de 2000 \nEstatura:1.71 \nPeso:70 kg ",
 		"Aimar":        "Edad: 43\nNombre completo: Pablo César Aimar Giordano \nApodo: El payaso Aimar \nFecha de nacimiento:  3 de noviembre de 1979 \nEstatura:  168 \nPeso: 69 kg",
 		"Tagliafico":   "Edad: 31\nNombre completo: Nicolás Alejandro \nApellido:Tagliafico\nApodo: taglia\nFecha de nacimiento: 31 de agosto de 1992\nEstatura: 172 cm\nPeso: 72 kg",
 		"Francescoli":  "Edad: 61\n Nombre completo: Enzo Francescoli \n Apodo: El Principe \n Fecha de nacimiento: 12 de noviembre de 1961 \n Estatura: 1.81 \n Peso: 73kg",
-		"Messi":        "Edad:36 \nNombre completo:Lionel Andres Messi Cuccittini \nApodo:La pulga \nFecha de nacimiento:24 de junio de 1987  \nEstatura:1.7 \nPeso:65 ",
+		"Messi":        "Edad: 36 \nNombre completo:Lionel Andres Messi Cuccittini \nApodo:La pulga \nFecha de nacimiento:24 de junio de 1987  \nEstatura:1.7 \nPeso:65 ",
 		"Mac Allister": "Edad: 24\nNombre completo:Alexis Mac Allister\nApodo: Macca\nFecha de nacimiento: 24 de diciembre de 1998\nEstatura: 1,76 m\nPeso: 72kg",
 		"Álvarez":      "Edad: 26\nNombre completo: Julian\nApellido: Álvarez\nApodo: araña\nFecha de nacimiento:  30 de enero de 2000\nEstatura:  170 cm\nPeso: 71 kg",
 		"Martínez":     "Edad: 31\nNombre completo: Damián Emiliano \nApellido: Martínez\nApodo: Dibu\nFecha de nacimiento: 2 de septiembre de 1992\nEstatura: 195 cm\nPeso: 88 kg",
